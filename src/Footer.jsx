99218330be6fe9be67286c803e7edd135c751156
Marin Marinov <marinm309@abv.bg>
2024-03-11 23:04:49 +0200

    asdf

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,23 +7,6 @@ function Footer(){
     return(
         <>
             <footer className="footer-container">
-                {/* <div className="footer-social-container">
-                    <a href="#" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
-                    <a href="#" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
-                    <a href="#" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-                    <a href="#" target="_blank"><FontAwesomeIcon icon={faTiktok} /></a>
-                </div>
-                <div className="footer-logo-container">
-                    <h2>LOGO</h2>
-                    <p>Copyright &reg; 2024 LOGO</p>
-                </div>
-                <div className="footer-info-container">
-                    <ul>
-                        <li><a href="#">About Us</a></li> |
-                        <li><a href="#">FAQs</a></li> |
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div> */}
                 <p>При интерес, моля свържете се с нас <Link>тук</Link></p>
             </footer>
         </>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,32 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
-
 function Header(props){
     
     return(
         <header>
             <div className="whole-nav-container">
                 <div className="container">
-                    {/* <div className="logo"><Link to={'/'}>Logo</Link></div> */}
                     <h4 className="header-msg-1"><i><b>Събрани на едно място автомобили за познавачи и ценители!</b></i></h4>
                     <h4 className="header-msg-2"><i><b>Вечните класики!</b></i></h4>
-                    {/* <form className="search-form" action="#" method="GET">
-                        <div className="search-bar">
-                            <input type="text" name="search" placeholder="Search..." />
-                            <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-                        </div>
-                    </form> */}
-                    {/* <nav className="nav-menu">
-                        <ul>
-                            <li><Link>Placeholder 1</Link></li>
-                            <li><Link>Placeholder 2</Link></li>
-                            <li><Link to={'/home2'} style={{color: 'pink', fontWeight: 'bold', backgroundColor: 'grey', borderRadius: '10px', padding: '5px'}}>Лили версия</Link></li>
-                        </ul>
-                    </nav> */}
-                    {/* <div className="burger-menu">
-                        <FontAwesomeIcon icon={faBars} />
-                    </div> */}
                 </div>
             </div>
         </header>
