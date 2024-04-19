a7147fd7bedf33daf8c1959e602ede81f60fc2cd
Marin Marinov <marinm309@abv.bg>
2024-04-19 03:23:59 +0300

    zoom

--- src/CarReview.jsx
+++ src/CarReview.jsx
@@ -2,125 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import ReactImageGallery from "react-image-gallery";
 import carInfo from './carInfo';
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
 
 const carReviews = carInfo
 
 const CarReview = () => {
-
-  function zoomHandler(){
-const mainContainer = document.querySelector('.image-gallery-slides')
-mainContainer.children[0].setAttribute('id', 'imageContainer')
-mainContainer.children[0].children[0].setAttribute('id', 'zoom-image')
-
-const mainDiv = document.getElementById('imageContainer');
-
-// Zoom On Scroll
-const image = document.getElementById('zoom-image');
-image.onload = createImage;
-
-let actualImageWidth;
-let actualImageHeight;
-
-function createImage() {
-    actualImageWidth = image.width;
-    actualImageHeight = image.height;
-    console.log("Actual Image Width and Height: ", [actualImageWidth, actualImageHeight]);
-}
-
-let zoomScrollX = 0;
-let zoomScrollY = 0;
-
-let zoomFactor = 1;
-image.addEventListener('wheel', function(event) {
-    event.preventDefault();
-    const xPos = event.clientX - image.getBoundingClientRect().left;
-    const yPos = event.clientY - image.getBoundingClientRect().top;
-
-    if (event.deltaY < 0) {
-        if (zoomFactor <= 4) {
-            console.log('zoomFactor Value: ', zoomFactor);
-            zoomFactor += 1;
-            console.log('Zoom Factor: ', zoomFactor);
-
-            checkZoom();
-
-            mainDiv.scrollTo(zoomScrollX, zoomScrollY);
-        } else {
-            console.log('Reached Max Zoom Level');
-        }
-    } else {
-        if (zoomFactor >= 2) {
-            console.log('zoomFactor Value: ', zoomFactor);
-            zoomFactor -= 1;
-            console.log('Zoom Factor: ', zoomFactor);
-            console.log('Zoom out...');
-
-            zoomScrollX = xPos * zoomFactor;
-            zoomScrollY = yPos * zoomFactor;
-
-            checkZoom();
-
-            mainDiv.scrollTo(zoomScrollX, zoomScrollY);
-        } else {
-            console.log('Reached Minimum Zoom Level');
-        }
-    }
-});
-
-// Function CheckZoom Level.
-function checkZoom() {
-    console.log('Zoom Level: ', zoomFactor);
-    console.log("Actual Image Width: ", actualImageWidth);
-    console.log("Actual Image Height: ", actualImageHeight);
-
-    const imageWidth = actualImageWidth * zoomFactor;
-    const imageHeight = actualImageHeight * zoomFactor;
-
-    image.style.width = imageWidth + 'px';
-    image.style.height = imageHeight + 'px';
-
-    console.log('Image Width: ', imageWidth);
-    console.log('Image Height: ', imageHeight);
-}
-
-// Pan Function 
-const Pannable = (EL) => {
-    let isPan = false;
-    let initial = { x: 0, y: 0 };
-
-    const getXY = ({ clientX, clientY }) => {
-        const bcr = EL.getBoundingClientRect();
-        return {
-            x: clientX - bcr.left,
-            y: clientY - bcr.top,
-        }
-    };
-
-    const panStart = (ev) => {
-        ev.preventDefault();
-        isPan = true;
-        const { x, y } = getXY(ev);
-        initial = { x: EL.scrollLeft + x, y: EL.scrollTop + y };
-    };
-
-    const panMove = (ev) => {
-        if (!isPan) return;
-        const { x, y } = getXY(ev);
-        EL.scrollTo(initial.x - x, initial.y - y);
-    };
-
-    const panEnd = (ev) => {
-        isPan = false;
-    };
-
-    EL.addEventListener("mousedown", panStart);
-    document.addEventListener("mousemove", panMove);
-    document.addEventListener("mouseup", panEnd);
-};
-
-document.querySelectorAll(".viewport").forEach(Pannable);
-
-  }
 
   const { carId } = useParams();
   const carInfo = carReviews[carId];
@@ -137,9 +23,11 @@ document.querySelectorAll(".viewport").forEach(Pannable);
   const onClickHandler = () => {
     document.querySelector('.image-gallery-fullscreen-button').click()
   };
-
   
+  // const { zoomIn, zoomOut, resetTransform } = useControls();
+
   return (
+    
     <section className="container flex-grow mx-auto max-w-[1200px] border-b py-5 lg:grid lg:grid-cols-2 lg:py-10 single-car-review">
       <div className="container mx-auto px-4">
         <ReactImageGallery
@@ -149,7 +37,23 @@ document.querySelectorAll(".viewport").forEach(Pannable);
           showPlayButton={false}
           items={images}
           slideDuration={0}
-          onClick={onClickHandler}
+          renderItem={(item) => (
+            <TransformWrapper 
+            disablePadding={true}
+            smooth={true}
+            panning={{
+                velocityDisabled: true
+            }}
+            wheel={{
+              smoothStep: 0.005,
+              step: 1
+            }}
+            >
+              <TransformComponent>
+                <img src={item.original} />
+              </TransformComponent>
+            </TransformWrapper>
+          )}
         />
 
       </div>
